a0d55b707c49c4383878cc9f13825def0a185638
MrLonzanida <mrlonzanida08@gmail.com>
2025-05-02 16:47:05 +0800

    add create task

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("todos/create", views.create_todo, name="create_todo"),
 ]
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from .models import Todo
+import uuid
 # Create your views here.
-
 def index(request):
+    todos = Todo.objects.all()
     return render(request, 'to_do_list/index.html', {
-        'message': 'This is your first template!'
+        'message': 'This is your first template!',
+        'todos': todos
     })
+
+def create_todo(request):
+    task = request.POST.get('task')
+    Todo.objects.create(
+        id=uuid.uuid4(),
+        task=task,
+        completed=False
+    )
+    return redirect('index')
