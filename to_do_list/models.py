38ef70bc599395a0f1727cb608baa24d87595b7c
MrLonzanida <mrlonzanida08@gmail.com>
2025-05-02 14:30:37 +0800

    fix Todo model

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Todo(models.Model):
     id = models.UUIDField(primary_key=True)
     task = models.CharField(max_length=255)
-    completed = models.BooleanField(False)
+    completed = models.BooleanField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
