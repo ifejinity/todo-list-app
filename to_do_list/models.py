285fa23aa9ed7c74b63cb95cf9b28e23a8b5746d
MrLonzanida <mrlonzanida08@gmail.com>
2025-05-02 13:58:28 +0800

    add my first template

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True)
+    task = models.CharField(max_length=255)
+    completed = models.BooleanField(False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
